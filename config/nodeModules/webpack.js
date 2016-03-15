9ecbde9b2b0b0980d08e76ab6f9f65ce5c9f7f05
Itadakimas <said.ahemt@gmail.com>
2016-03-15 15:07:49 +0100

    removes commons chunk plugin from webpack config

--- config/nodeModules/webpack.js
+++ config/nodeModules/webpack.js
@@ -17,7 +17,6 @@ const PLUGINS = {
   development: [
   ],
   distributable: [
-    new Webpack.optimize.CommonsChunkPlugin({name: "vendors", filename: "vendors.js"}),
     new Webpack.optimize.UglifyJsPlugin()
   ]
 };
@@ -26,38 +25,35 @@ const PLUGINS = {
  * Webpack configuration
  */
 module.exports = {
-    devtool: (argv.mode === "development") ? "inline-source-map" : null,
-    entry: {
-      main: Path.resolve(PROJECT_DIR, "src/js/main.js"),
-      vendors: ["eventEmitter", "rlite", "svg4everybody", "vue"]
-    },
-    output: {
-      filename: "[name].js",
-      chunkFilename: "[id].js"
-    },
-    module: {
-        loaders: [
-            { test: /\.js$/, exclude: /(node_modules|vendors)/, loader: "babel-loader"},
-            { test: /\.html$/, exclude: /(node_modules|vendors)/, loader: "html-loader?attrs=false"}
-        ]
-    },
-    resolve: {
-        alias: {
-            /*
-             * Vendors
-             */
-            eventEmitter: Path.resolve(PROJECT_DIR, "src/vendors/eventEmitter/EventEmitter.min.js"),
-            rlite: Path.resolve(PROJECT_DIR, "src/vendors/rlite/rlite.min.js"),
-            svg4everybody: Path.resolve(PROJECT_DIR, "src/vendors/svg4everybody/dist/svg4everybody.min.js"),
-            vue: Path.resolve(PROJECT_DIR, "src/vendors/vue/dist/vue.min.js"),
+  devtool: (argv.mode === "development") ? "inline-source-map" : null,
+  entry: Path.resolve(PROJECT_DIR, "src/js/main.js"),
+  output: {
+    filename: "[name].js",
+    chunkFilename: "[id].js"
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /(node_modules|vendors)/, loader: "babel-loader"},
+      { test: /\.html$/, exclude: /(node_modules|vendors)/, loader: "html-loader?attrs=false"}
+    ]
+  },
+  resolve: {
+    alias: {
+      /*
+       * Vendors
+       */
+      eventEmitter: Path.resolve(PROJECT_DIR, "src/vendors/eventEmitter/EventEmitter.min.js"),
+      rlite: Path.resolve(PROJECT_DIR, "src/vendors/rlite/rlite.min.js"),
+      svg4everybody: Path.resolve(PROJECT_DIR, "src/vendors/svg4everybody/dist/svg4everybody.min.js"),
+      vue: Path.resolve(PROJECT_DIR, "src/vendors/vue/dist/vue.min.js"),
 
-            /*
-             * Directories
-             */
-            core: Path.resolve(PROJECT_DIR, "src/js/core"),
-            modules: Path.resolve(PROJECT_DIR, "src/js/modules"),
-            sections: Path.resolve(PROJECT_DIR, "src/sections")
-        }
-    },
-    plugins: PLUGINS[argv.mode]
+      /*
+       * Directories
+       */
+      core: Path.resolve(PROJECT_DIR, "src/js/core"),
+      modules: Path.resolve(PROJECT_DIR, "src/js/modules"),
+      sections: Path.resolve(PROJECT_DIR, "src/sections")
+    }
+  },
+  plugins: PLUGINS[argv.mode]
 };
