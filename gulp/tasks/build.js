2cea90f5b75ebeb9cbc5d084529171cc283351b2
Itadakimas <said.ahemt@gmail.com>
2016-06-04 18:48:05 +0200

    generates build.json file in dist directory

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -3,11 +3,14 @@
  */
 var Colors = require("colors/safe");
 var Gulp = require("gulp");
+var JSONFile = require("jsonfile");
+var Package = require("../../package.json");
 var RunSequence = require("run-sequence");
 
 /*
  * Modules
  */
+var argv = require("../modules/argv");
 var paths = require("../modules/paths");
 
 /*
@@ -17,7 +20,27 @@ Gulp.task("build", function(callback) {
 
   return RunSequence(["sass", "svg", "html", "jade", "javascript", "static", "images"], function() {
 
-    callback();
-    console.log("👍  " + Colors.bold.green("Completed successfully! Your build is available in the following directory: ") + paths.relocate("dist"));
+    var buildData,
+        buildDataFile;
+
+    buildData = {
+      date: new Date().toISOString(),
+      env:  argv.env,
+      name: Package.name
+    };
+    buildDataFile = paths.relocate("dist/build.json");
+    JSONFile.writeFile(buildDataFile, buildData, function(err) {
+
+      if (err)
+      {
+        console.log("❗  " + Colors.bold.red("Couldn't write the following file: ") + buildDataFile);
+        console.log(err);
+      }
+      else
+      {
+        console.log("👍  " + Colors.bold.green("Completed successfully! Your build is available in the following directory: ") + paths.relocate("dist"));
+      }
+      callback();
+    });
   });
 });
