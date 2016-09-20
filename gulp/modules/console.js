a65d473c9ddd7ae918de627136450ed173338182
Itadakimas <said.ahemt@gmail.com>
2016-09-20 15:11:42 +0200

    fixes colored logs in Jetbrains IDEs

--- gulp/modules/console.js
+++ gulp/modules/console.js
@@ -1,37 +1,37 @@
 /*
  * Dependencies
  */
-var Colors = require("colors/safe");
+var chalk = require('chalk');
 var GulpUtil = require("gulp-util");
 
 module.exports = {
-  
+
   log: function() {
 
     GulpUtil.log.apply(null, arguments);
   },
   error: function(msg) {
 
-    this.log(Colors.red("[ERROR] " + msg));
+    this.log(chalk.red("[ERROR] " + msg));
   },
   info: function(msg) {
 
-    this.log(Colors.cyan("[INFO] " + msg));
+    this.log(chalk.blue("[INFO] " + msg));
   },
   raw: function() {
-    
+
     console.log.apply(null, arguments);
   },
   success: function(msg) {
 
-    this.log(Colors.green("[SUCCESS] " +  msg));
+    this.log(chalk.green("[SUCCESS] " +  msg));
   },
   trace: function(err) {
-    
+
     console.trace(err);
   },
   warning: function(msg) {
 
-    this.log(Colors.yellow("[WARNING] " + msg));
+    this.log(chalk.yellow("[WARNING] " + msg));
   }
 };
