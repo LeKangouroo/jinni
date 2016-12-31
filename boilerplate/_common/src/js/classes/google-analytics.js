d6981411eb031d2548374d2aa1674b84c040766a
Itadakimas <said.ahemt@gmail.com>
2016-12-31 23:42:25 +0100

    rewrites Google Analytics class

--- boilerplate/_common/src/js/classes/google-analytics.js
+++ boilerplate/_common/src/js/classes/google-analytics.js
@@ -1,51 +1,55 @@
-/* jshint sub:true */
-/* jshint expr:true */
-/* globals ga */
-export default class GoogleAnalytics
+class GoogleAnalytics
 {
-  static init(params) {
+  /**
+   * Initializes Google Analytics tracking
+   * @param {object} options
+   * @param {string} options.trackingID UA Tracking ID
+   */
+  static init(options)
+  {
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-    if (!this.isInitialized())
-    {
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments);},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m);
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-      
-      ga('create', params.ua, 'auto');
-    }
+    ga('create', options.trackingID, 'auto');
   }
-  static isInitialized() {
-    
-    return (typeof window.ga !== "undefined");
-  }
-  static sendEvent(options) {
 
-    if (!this.isInitialized())
-    {
-      return;
-    }
-    window.ga("send", "event", options.category, options.action, options.label, options.value, {
+  /**
+   * Sends an event hit
+   * @param {object} options
+   * @param {string} options.category the event's category
+   * @param {string} options.action the event's action
+   * @param {string} options.label the event's label
+   * @param {string|undefined} options.value the event's value
+   */
+  static sendEvent(options)
+  {
+    ga('send', 'event', options.category, options.action, options.label, options.value, {
       nonInteraction: true
     });
   }
-  static sendPageView(options) {
-    
-    var trackerParams;
 
-    if (!this.isInitialized())
-    {
-      return;
-    }
-    trackerParams = {};
-    if (typeof options.page === "string")
+  /**
+   * Sends a page view hit
+   * @param {object} options
+   * @param {string} options.page the url of the page
+   * @param {string} options.title the title of the page
+   */
+  static sendPageView(options)
+  {
+    const trackerParams = {};
+
+    if (typeof options.page === 'string')
     {
       trackerParams.page = options.page;
     }
-    if (typeof options.title === "string")
+    if (typeof options.title === 'string')
     {
       trackerParams.title = options.title;
     }
-    window.ga("send", "pageview", trackerParams);
+    ga('send', 'pageview', trackerParams);
   }
 }
+
+export default GoogleAnalytics;
