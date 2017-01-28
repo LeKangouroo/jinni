b89a9a855c1435ee61a1687d25abbbe10bf76916
Itadakimas <said.ahemt@gmail.com>
2017-01-28 21:54:04 +0100

    fixes error when no command is provided using CLI

--- src/bin/cli.js
+++ src/bin/cli.js
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
 const usage = require('../usages/default');
 const argv = usage.argv;
-const cmd = argv._[0];
+const cmd = (typeof argv._[0] === 'string') ? argv._[0] : null;
 const pkg = require('../../package.json');
-const script = `../scripts/${cmd}`;
 
 if (argv.version)
 {
   console.log(`${pkg.name} v${pkg.version}`);
   process.exit(0);
 }
-require(script);
+if (!cmd)
+{
+  usage.showHelp();
+  process.exit(1);
+}
+require(`../scripts/${cmd}`);
