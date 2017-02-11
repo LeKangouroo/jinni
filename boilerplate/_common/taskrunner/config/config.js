febf988d4a5cd9e7e72a4d369e8bc146c206b94e
Itadakimas <said.ahemt@gmail.com>
2017-02-11 15:09:04 +0100

    replaces Jasmine with Mocha and Chai

--- boilerplate/_common/taskrunner/config/config.js
+++ boilerplate/_common/taskrunner/config/config.js
@@ -1,10 +1,10 @@
 import autoPrefixer from './nodeModules/auto-prefixer.json';
 import browserSync from './nodeModules/browser-sync.json';
 import clean from './tasks/clean.json';
-import jasmine from './nodeModules/jasmine';
 import JPEGRecompress from './nodeModules/imagemin/jpeg-recompress.json';
 import jshint from './nodeModules/jshint.json';
 import jsonServer from './nodeModules/json-server.json';
+import mocha from './nodeModules/mocha';
 import paths from './common/paths.json';
 import PNGQuant from './nodeModules/imagemin/pngquant.json';
 import pug from './nodeModules/pug.json';
@@ -24,10 +24,10 @@ export default {
     autoPrefixer,
     browserSync,
     imagemin: { PNGQuant, JPEGRecompress },
-    pug,
-    jasmine,
     jshint,
     jsonServer,
+    mocha,
+    pug,
     sass,
     svgSprite,
     webpack
--- boilerplate/_common/taskrunner/tasks/instrumented-tests.js
+++ boilerplate/_common/taskrunner/tasks/instrumented-tests.js
@@ -1,11 +1,13 @@
 import config from '../config/config';
 import gulp from 'gulp';
-import jasmine from 'gulp-jasmine';
+import mocha from 'gulp-mocha';
 import paths from '../modules/paths';
 
 gulp.task('instrumented-tests', () => {
 
   gulp
     .src(paths.relocate(config.common.paths.sources.instrumentedTests))
-    .pipe(jasmine(config.nodeModules.jasmine));
+    .pipe(mocha(config.nodeModules.mocha))
+    .once('error', () => process.exit(1))
+    .once('end', () => process.exit());
 });
