5a94370321874690ff81ce9d2e4d23e227f63d8c
Itadakimas <said.ahemt@gmail.com>
2017-08-25 18:42:52 +0200

    adds unit tests for the routing.serializeURLSearch method

--- tests/boilerplate/modules/network/routing.spec.js
+++ tests/boilerplate/modules/network/routing.spec.js
@@ -3,6 +3,33 @@ const routing = require("../../../../boilerplate/_common/src/js/modules/network/
 
 describe("Modules > Network > Routing", function() {
 
+  describe("serializeURISearch()", function() {
+
+    it("should return the correct string with a simple use case", function() {
+
+      const search = { foo: "bar", toto: 4, isOK: true };
+      const expectedResult = "?foo=bar&toto=4&isOK=true";
+
+      assert.deepStrictEqual(routing.serializeURISearch(search), expectedResult);
+    });
+
+    it("should return the correct string with falsy values", function() {
+
+      const search = { a: false, b: 0, c: null, d: undefined, e: "" };
+      const expectedResult = "?a=false&b=0&c&e=";
+
+      assert.deepStrictEqual(routing.serializeURISearch(search), expectedResult);
+    });
+
+    it("should return the correct string with nested lists", function() {
+
+      const search = { a: [1,2,3], b: [1,2,3] };
+      const expectedResult = "?a=1&a=2&a=3&b=1&b=2&b=3";
+
+      assert.deepStrictEqual(routing.serializeURISearch(search), expectedResult);
+    });
+  });
+
   describe("unserializeURISearch()", function() {
 
     it("should return an object with key value pairs", function() {
