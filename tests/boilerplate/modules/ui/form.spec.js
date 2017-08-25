887fe23068f1852e9fa9ac6ffdc4d3dcc94ff22b
Itadakimas <said.ahemt@gmail.com>
2017-08-25 23:37:20 +0200

    adds unit tests for the form.isFormElement() method

---
+++ tests/boilerplate/modules/ui/form.spec.js
@@ -0,0 +1,51 @@
+const assert = require("assert");
+const form = require("../../../../boilerplate/_common/src/js/modules/ui/form");
+const JSDOM = require("jsdom").JSDOM;
+const HTMLFormElement = require("jsdom/lib/jsdom/living/generated/HTMLFormElement").interface;
+
+global.HTMLFormElement = HTMLFormElement;
+
+const createTestForm = () => {
+
+  const dom = new JSDOM(`
+<!doctype html>
+<html>
+<head></head>
+<body>
+  <form>
+    <div>
+      <input type="radio" name="gender" value="male">
+      <input type="radio" name="gender" value="female">
+    </div>
+    <div>
+      <input type="checkbox" name="skills" value="cooking">
+      <input type="checkbox" name="skills" value="flying">
+      <input type="checkbox" name="skills" value="cqc">
+    </div>
+    <input type="text" name="lastname">
+    <input type="text" name="firstname">
+    <textarea name="message"></textarea>
+  </form>
+</body>
+</html>`);
+
+  const document = dom.window.document;
+
+  return document.querySelector("form");
+};
+
+describe("Modules > UI > Form", function() {
+
+  describe("isFormElement()", function() {
+
+    it("should validate that it's a form element", function() {
+
+      assert.strictEqual(form.isFormElement(createTestForm()), true);
+    });
+
+    it("should not validate that it's a form element", function() {
+
+      assert.strictEqual(form.isFormElement({}), false);
+    });
+  });
+});
