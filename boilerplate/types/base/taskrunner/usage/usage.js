285c58214066daf8a131f551acae8d88cf22fff7
Itadakimas <services@lekangouroo.me>
2018-11-04 19:12:15 +0100

    removes unused code

--- boilerplate/types/base/taskrunner/usage/usage.js
+++ boilerplate/types/base/taskrunner/usage/usage.js
@@ -1,19 +1,10 @@
 import build from './commands/build';
 import clean from './commands/clean';
 import dev from './commands/dev';
-import fs from 'fs';
 import yargs from 'yargs';
 import zip from './commands/zip';
 
-/*
- * Optional commands
- */
-const API_COMMAND_PATH = './commands/api';
-
-/*
- * Base configuration
- */
-const usage = yargs
+export default yargs
   .locale('en')
   .usage('Usage: $0 <command> [options]')
   .command(build)
@@ -22,13 +13,3 @@ const usage = yargs
   .command(zip)
   .alias('help', 'h')
   .help();
-
-/*
- * REST API feature configuration
- */
-if (fs.existsSync(API_COMMAND_PATH))
-{
-  usage.command(require(API_COMMAND_PATH));
-}
-
-export default usage;
