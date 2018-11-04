fabd2dfb4dab4b83bba01dc7d6aafbfbbf32fd93
Itadakimas <services@lekangouroo.me>
2018-11-04 15:24:33 +0100

    fixes unit tests

--- boilerplate/types/base/taskrunner/usage/usage.js
+++ boilerplate/types/base/taskrunner/usage/usage.js
@@ -2,7 +2,6 @@ import build from './commands/build';
 import clean from './commands/clean';
 import dev from './commands/dev';
 import fs from 'fs';
-import unitTests from "./commands/unit-tests";
 import yargs from 'yargs';
 import zip from './commands/zip';
 
@@ -20,7 +19,6 @@ const usage = yargs
   .command(build)
   .command(clean)
   .command(dev)
-  .command(unitTests)
   .command(zip)
   .alias('help', 'h')
   .help();
