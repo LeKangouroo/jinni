a023e5d2bce82320061b032ef3208a1f1178d04d
LeKangouroo <contact@lekangouroo.me>
2020-09-04 19:52:21 +0200

    remove svg4everybody dependency

--- boilerplate/types/spa/src/js/main.js
+++ boilerplate/types/spa/src/js/main.js
@@ -1,12 +1,9 @@
 import events from "core/events";
 import homeSection from "sections/home/home";
 import router from "core/router";
-import SVG4Everybody from "svg4everybody"; // TODO: remove this dependency
 import Vue from "vue";
 
 console.log("main.js file loaded");
-
-SVG4Everybody(); // TODO: remove this dependency
 
 document.addEventListener("DOMContentLoaded", () => {
 
@@ -33,7 +30,6 @@ document.addEventListener("DOMContentLoaded", () => {
       events.addObserver("section:loaded", () => {
 
         this.isLoading = false;
-        SVG4Everybody();
       });
 
       events.addObserver("section:destroyed", () => {
