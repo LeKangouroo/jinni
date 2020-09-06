8be948d8553dea2941a128e20707056e718b9d83
LeKangouroo <contact@lekangouroo.me>
2020-09-06 03:14:15 +0200

    add page title definition for SPA boilerplate

--- boilerplate/types/spa/src/js/core/router.js
+++ boilerplate/types/spa/src/js/core/router.js
@@ -1,14 +1,27 @@
-import { createRouter } from "modules/network/routing";
-import events from "core/events";
+import { createRouter } from "modules/network/routing.js";
+import events from "core/events.js";
+
+const getTitle = (sectionName, env) => env === "production"
+  ? `${sectionName} - ${window.location.hostname}`
+  : `[${env}] ${sectionName} - ${window.location.hostname}`;
 
 const routes = [
-  { name: "home", uri: "/home" }
+  {
+    name: "home",
+    uri: "/home",
+    data: {
+      title: getTitle("Accueil", "/* @echo ENV*/")
+    }
+  }
 ];
 
 const router = createRouter(window, routes);
 
 router.setDefaultRoute("home");
+router.onRouteChange(route => {
 
-router.onRouteChange((route) => events.notifyObservers("router:update", route));
+  document.title = route.data.title;
+  events.notifyObservers("router:update", route);
+});
 
 export default router;
